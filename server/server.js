82c9162965f8814989a08bbf72c24865396c8edd
Marto <martinkaldramov1@gmail.com>
2017-09-18 22:03:11 +0300

    Created a User collection and saved a new user to it

--- server/server.js
+++ server/server.js
@@ -15,6 +15,25 @@ var Todo = mongoose.model('Todo', {
   }
 });
 
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }  
+});
+
+var newUser = new User({
+  email: '  martinkaldramov@yahoo.com  '  
+});
+
+newUser.save().then((doc) => {
+  console.log('User successfully saved to the database', doc);
+}, (e) => {
+  console.log('Unable to save the user to the database', e);
+});
+
 // var newTodo = new Todo({
 //   text: 'Cook Dinner'  
 // });
@@ -26,14 +45,14 @@ var Todo = mongoose.model('Todo', {
 // });
 // 
 
-var otherTodo = new Todo({
-  text: 'Feed the dog',
-  completed: false,
-  completedAt: 1234
-});
-
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save the todo', e);
-});
+// var otherTodo = new Todo({
+//   text: 'Feed the dog',
+//   completed: false,
+//   completedAt: 1234
+// });
+// 
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save the todo', e);
+// });
