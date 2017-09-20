68ed8a7299dcdd14c6c9abfab93e1fb0a99f440b
Marto <martinkaldramov1@gmail.com>
2017-09-20 14:01:10 +0300

    Setting up unit test for posting to the Todos database (installed expect, supertest and mocha)

---
+++ server/tests/server.test.js
@@ -0,0 +1,9 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require(./../server.js);
+const {Todo} = require('./../models/Todo');
+
+describe('POST /todos', () => {
+    
+});
