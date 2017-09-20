1d2bacef585684b7c8cc93972c13fd8a6225e6a5
Marto <martinkaldramov1@gmail.com>
2017-09-20 07:13:47 +0300

    Installed Express and Body Parser and created a basic server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
+
+var app = express();
+
+app.listen(300, () => {
+  console.log('Listening on port 3000');  
+});
